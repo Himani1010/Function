05547e22e98a86645e146b8e50525c68102017a9
Himani1010 <84378418+Himani1010@users.noreply.github.com>
2021-07-27 20:16:27 +0530

    Add files via upload

--- first_question.py
+++ first_question.py
@@ -1,7 +1,14 @@
-def ask_question():
-    print("Hello")
-ask_question()
-ask_question()
-ask_question()
-ask_question()
-ask_question()
+def my_function(list):
+    i=0
+    count=0
+    while i<len(list):
+        if list[i][0]==list[i][-1]:
+            count+=1
+        i+=1
+        # b=str(list[i])
+        # a=str(list[i])
+        # if b[0]==a[0]:
+        #     count=count+1
+    print(count)
+list=['abc', 'xyz', 'aba', '1221','bcb']
+my_function(list)
